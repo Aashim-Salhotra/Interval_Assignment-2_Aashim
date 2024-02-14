f0d65346eb54cc8671cd7d9f09c681f28a8e72ee
Aashim-Salhotra <157233733+Aashim-Salhotra@users.noreply.github.com>
2024-02-13 22:15:44 -0500

    Add files via upload

---
+++ Interval_Assignment-2_Aashim/Interval_Assignment-2_Aashim/Program.cs
@@ -0,0 +1,13 @@
+﻿//Aashim Salhotra C0911959
+//Assignment 2: 
+
+Console.Write("Please enter a real number: ");
+double x = double.Parse(Console.ReadLine());
+
+bool belongs = (x >= 2 && x < 3) || (x >= 0 && x <= 1) || (x >= -10 && x <= -2);
+
+if (belongs)
+{Console.WriteLine("x belongs to I");}
+else
+{Console.WriteLine("x does not belong to I");}
+
